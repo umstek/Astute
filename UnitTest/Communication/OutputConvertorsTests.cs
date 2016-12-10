511cd13aac45442130aa2d7c5bf54ad07d25f383
Wickramaranga <umstek@live.com>
2016-12-10 13:13:02 +0530

    Tests on OutputConvertors

---
+++ UnitTest/Communication/OutputConvertorsTests.cs
@@ -0,0 +1,32 @@
+﻿using Astute.Communication;
+using Astute.Entity;
+using NUnit.Framework;
+
+namespace UnitTest.Communication
+{
+    [TestFixture]
+    public class OutputConvertorsTests
+    {
+        [Test]
+        public void CommandToStringTests()
+        {
+            var shoot = OutputConvertors.CommandToString(Command.Shoot);
+            Assert.AreEqual("SHOOT#", shoot);
+
+            var left = OutputConvertors.CommandToString(Command.Left);
+            Assert.AreEqual("LEFT#", left);
+
+            var right = OutputConvertors.CommandToString(Command.Right);
+            Assert.AreEqual("RIGHT#", right);
+
+            var up = OutputConvertors.CommandToString(Command.Up);
+            Assert.AreEqual("UP#", up);
+
+            var down = OutputConvertors.CommandToString(Command.Down);
+            Assert.AreEqual("DOWN#", down);
+
+            var join = OutputConvertors.CommandToString(Command.Join);
+            Assert.AreEqual("JOIN#", join);
+        }
+    }
+}
