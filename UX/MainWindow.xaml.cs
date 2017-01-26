99cb8b8c31bb1679a6a11d107c6b8151e5a4a21d
Wickramaranga <umstek@live.com>
2017-01-26 11:05:26 +0530

    Base UI

--- UX/MainWindow.xaml.cs
+++ UX/MainWindow.xaml.cs
@@ -1,119 +1,107 @@
 ﻿using System;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Shapes;
-using Astute.Communication;
 using Astute.Communication.Messages;
 using Astute.Engine;
 using Astute.Entity;
-using NLog;
+using MahApps.Metro.Controls;
 
 namespace UX
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
-        private readonly Engine _engine = new AI();
-        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
-
         public MainWindow()
         {
             InitializeComponent();
-            Observable.FromEventPattern<KeyEventHandler, KeyEventArgs>(
-                    handler => KeyDown += handler,
-                    handler => KeyDown -= handler)
-                .Select(pattern =>
-                {
-                    switch (pattern.EventArgs.Key)
-                    {
-                        case Key.Enter:
-                            Task.Run(() =>
-                                Input.TcpInput.Retry().Select(MessageFactory.GetMessage)
-                                    .Do(_engine.ReceiveMessage)
-                                    .Subscribe(_ => UpdateGridUI()));
-                            return Command.Join;
-                        case Key.A: // Fallthrough
-                        case Key.Left:
-                            return Command.Left;
-                        case Key.D: // Fallthrough
-                        case Key.Right:
-                            return Command.Right;
-                        case Key.W: // Fallthrough
-                        case Key.Up:
-                            return Command.Up;
-                        case Key.S: // Fallthrough
-                        case Key.Down:
-                            return Command.Down;
-                        default:
-                            return Command.Shoot;
-                    }
-                })
-                .Select(OutputConvertors.CommandToString)
-                .Subscribe(Output.TcpOutput);
+
+            SubscriptionManager.StateAndMessageStream.Subscribe(
+                stateAndMessage => RenderGrid(stateAndMessage.Item1, stateAndMessage.Item2)
+            );
         }
 
-        private void UpdateGridUI()
+        private void RenderGrid(World world, IMessage message)
         {
-            if (Dispatcher.CheckAccess())
-                for (var i = 0; i < 20; i++)
-                for (var j = 0; j < 20; j++)
-                {
-                    var gi = _engine.State.GridItems[i, j];
-                    if (gi is Tank)
+            if (Dispatcher.CheckAccess()) // Accessing from UI thread. 
+                foreach (var slot in world.GridItems)
+                    switch (slot.GetType().Name)
                     {
-                        GradientBrush gradientBrush;
-                        switch ((gi as Tank).Direction)
-                        {
-                            case Direction.North:
-                                gradientBrush = new LinearGradientBrush(Colors.DarkGreen, Colors.WhiteSmoke, 90);
-                                break;
-                            case Direction.East:
-                                gradientBrush = new LinearGradientBrush(Colors.WhiteSmoke, Colors.DarkGreen, 0);
-                                break;
-                            case Direction.South:
-                                gradientBrush = new LinearGradientBrush(Colors.WhiteSmoke, Colors.DarkGreen, 90);
-                                break;
-                            case Direction.West:
-                                gradientBrush = new LinearGradientBrush(Colors.DarkGreen, Colors.WhiteSmoke, 0);
-                                break;
-                            default:
-                                throw new ArgumentOutOfRangeException();
-                        }
-                        ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = gradientBrush;
+                        case nameof(Tank):
+                            break;
+                        case nameof(BrickWall):
+                            break;
+                        case nameof(StoneWall):
+                            break;
+                        case nameof(Coinpack):
+                            break;
+                        case nameof(Lifepack):
+                            break;
+                        case nameof(Water):
+                            break;
+                        default:
+                            break;
                     }
-                    else if (gi is BrickWall)
-                    {
-                        ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Brown;
-                    }
-                    else if (gi is StoneWall)
-                    {
-                        ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Gray;
-                    }
-                    else if (gi is Coinpack)
-                    {
-                        ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Gold;
-                    }
-                    else if (gi is Lifepack)
-                    {
-                        ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Red;
-                    }
-                    else if (gi is Water)
-                    {
-                        ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Blue;
-                    }
-                    else
-                    {
-                        ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.White;
-                    }
-                }
             else
-                Dispatcher.Invoke(UpdateGridUI);
+                Dispatcher.Invoke(() => RenderGrid(world, message));
         }
+
+        //{
+
+        //private void UpdateGridUI()
+        //    if (Dispatcher.CheckAccess())
+        //        for (var i = 0; i < 20; i++)
+        //        for (var j = 0; j < 20; j++)
+        //        {
+        //            var gi = _engine.State.GridItems[i, j];
+        //            if (gi is Tank)
+        //            {
+        //                GradientBrush gradientBrush;
+        //                switch ((gi as Tank).Direction)
+        //                {
+        //                    case Direction.North:
+        //                        gradientBrush = new LinearGradientBrush(Colors.DarkGreen, Colors.WhiteSmoke, 90);
+        //                        break;
+        //                    case Direction.East:
+        //                        gradientBrush = new LinearGradientBrush(Colors.WhiteSmoke, Colors.DarkGreen, 0);
+        //                        break;
+        //                    case Direction.South:
+        //                        gradientBrush = new LinearGradientBrush(Colors.WhiteSmoke, Colors.DarkGreen, 90);
+        //                        break;
+        //                    case Direction.West:
+        //                        gradientBrush = new LinearGradientBrush(Colors.DarkGreen, Colors.WhiteSmoke, 0);
+        //                        break;
+        //                    default:
+        //                        throw new ArgumentOutOfRangeException();
+        //                }
+        //                ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = gradientBrush;
+        //            }
+        //            else if (gi is BrickWall)
+        //            {
+        //                ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Brown;
+        //            }
+        //            else if (gi is StoneWall)
+        //            {
+        //                ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Gray;
+        //            }
+        //            else if (gi is Coinpack)
+        //            {
+        //                ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Gold;
+        //            }
+        //            else if (gi is Lifepack)
+        //            {
+        //                ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Red;
+        //            }
+        //            else if (gi is Water)
+        //            {
+        //                ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.Blue;
+        //            }
+        //            else
+        //            {
+        //                ((Rectangle) ((StackPanel) MainStack.Children[i]).Children[j]).Fill = Brushes.White;
+        //            }
+        //        }
+        //    else
+        //        Dispatcher.Invoke(UpdateGridUI);
+        //}
     }
 }
